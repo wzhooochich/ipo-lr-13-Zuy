b6d03ee4687d518a438208be3bb15fd204844683
wzhooochich <zujnikita867@gmail.com>
2024-12-20 22:44:36 +0300

    Update imageProcessor.py

--- image/imageProcessor.py
+++ image/imageProcessor.py
@@ -10,27 +10,20 @@ class ImageProcessor:
         """
         if self.img:
             if self.img.mode not in ("RGB", "RGBA"):
-                self.img = self.img.convert("RGB")
-            self.img = self.img.filter(ImageFilter.SHARPEN)
+               img = self.img.convert("RGB") 
+            else:
+               img = self.img
+            return img.filter(ImageFilter.SHARPEN)
         else:
             print("Изображение не загружено!")
-        return self.img # Возвращаем измененное изображение
+            return self.img
     
     def border(self, border_width=15, border_color="black"):
          """
          Добавить рамку вокруг изображения.
          """
          if self.img:
-              self.img = ImageOps.expand(self.img, border=border_width, fill=border_color)
+             return ImageOps.expand(self.img, border=border_width, fill=border_color)
          else:
               print("Изображение не загружено!")
-         return self.img # Возвращаем измененное изображение
-
-    def save_image(self, save_path):
-        """
-        Сохранить обработанное изображение.
-        """
-        if self.img:
-            self.img.save(save_path)
-        else:
-            print("Изображение не загружено!")
+              return self.img
